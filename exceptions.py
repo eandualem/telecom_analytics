335aff41b2691a7111e7a3bde564518773daac43
Elias <eandualem@gmail.com>
2021-07-15 09:46:40 +0300

    chore: add custom exception class

---
+++ exceptions.py
@@ -0,0 +1,16 @@
+class ObjectNotFound(Exception):
+  """
+      An exception risen when an expected object is not found
+  """
+
+class InvalidOperations(Exception):
+  """
+      An exception risen when attempted operation does not make sense
+  """
+
+
+class InvalidNumberOfArguments(Exception):
+  """
+      An exception risen when expected number of arguments were provided
+  """
+
