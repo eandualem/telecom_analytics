3df13cb8ac0abc99bbfd946469a4d79fb9665c74
Elias <eandualem@gmail.com>
2021-07-16 21:18:39 +0300

    feat: add more ploting functions

--- scripts/vis_plotly.py
+++ scripts/vis_plotly.py
@@ -1,6 +1,7 @@
 import plotly.express as px
 import plotly.graph_objects as go
 import plotly.figure_factory as ff
+from plotly.subplots import make_subplots
 
 def myLayout(title, x_title, y_title, mode, width, height, margin):
     return go.Layout(
@@ -64,3 +65,19 @@ def histogram(hist_data, group_labels, bin_size=1, curve_type='normal', show_his
                              curve_type=curve_type, show_hist=show_hist)
     fig.update_layout()
     fig.show()
+
+
+def hist(sr):
+  x = ["Id: " + str(i) for i in sr.index]
+  fig = px.histogram(x=x, y=sr.values)
+  fig.show()
+
+
+def mult_hist(sr, rows, cols, title_text, subplot_titles):
+  fig = make_subplots(rows=rows, cols=cols, subplot_titles=subplot_titles)
+  for i in range(rows):
+    for j in range(cols):
+      x = ["-> " + str(i) for i in sr[i+j].index]
+      fig.add_trace(go.Bar(x=x, y=sr[i+j].values ), row=i+1, col=j+1)
+  fig.update_layout(showlegend=False, title_text=title_text)
+  fig.show()
