54c84d60f8a93d34337d2a1267844537edb5e4fc
Elias <eandualem@gmail.com>
2021-07-18 17:23:29 +0300

    fix: add option to make plots non interactive

--- scripts/vis_plotly.py
+++ scripts/vis_plotly.py
@@ -2,6 +2,9 @@ import plotly.express as px
 import plotly.graph_objects as go
 import plotly.figure_factory as ff
 from plotly.subplots import make_subplots
+import plotly.io as pio
+from IPython.display import Image
+
 
 def myLayout(title, x_title, y_title, mode, width, height, margin):
     return go.Layout(
@@ -31,7 +34,7 @@ def BarTrace(x, y, names):
     return trace
 
 
-def barChart(x, y, names, title="", x_title="x", y_title="y", mode='group', full=False, static=True):
+def barChart(x, y, names, title="", x_title="x", y_title="y", mode='group', full=False, interactive=False):
     width = None
     height = None
     margin = None
@@ -42,47 +45,63 @@ def barChart(x, y, names, title="", x_title="x", y_title="y", mode='group', full
     trace = BarTrace(x=x, y=y, names=names)
     fig = go.Figure(data=trace, layout=myLayout(
         title, x_title, y_title, mode, width, height, margin))
-    config = {'staticPlot': static}
-    fig.show(config=config)
+
+    if(interactive):
+        fig.show()
+    else:
+        return Image(pio.to_image(fig, format='png', width=1200))
 
 
-def scatter(df, x, y, c=None, s=None, mx=None, my=None, af=None, fit=None, static=True):
+def scatter(df, x, y, c=None, s=None, mx=None, my=None, af=None, fit=None, interactive=False):
     fig = px.scatter(df, x=x, y=y, color=c, size=s, marginal_y=my,
                      marginal_x=mx, trendline=fit, animation_frame=af)
-    config = {'staticPlot': static}
-    fig.show(config=config)
+    if(interactive):
+        fig.show()
+    else:
+        return Image(pio.to_image(fig, format='png', width=1200))
 
 
-def scatter3D(df, x, y, z, c=None, s=None, mx=None, my=None, af=None, fit=None, static=True):
+def scatter3D(df, x, y, z, c=None, s=None, mx=None, my=None, af=None, fit=None, rotation=[1, 1, 1], interactive=False):
     fig = px.scatter_3d(df, x=x, y=y, z=z, color=c, size=s,
                         animation_frame=af, size_max=18)
-    config = {'staticPlot': static}
-    fig.show(config=config)
+
+    fig.update_layout(scene=dict(camera=dict(eye=dict(x=rotation[0], y=rotation[1], z=rotation[2]))),
+                      )
+    if(interactive):
+        fig.show()
+    else:
+        return Image(pio.to_image(fig, format='png', width=1200))
 
 
-def histogram(hist_data, group_labels, bin_size=1, curve_type='normal', show_hist=True, invert=False, static=True):
+def histogram(hist_data, group_labels, bin_size=1, curve_type='normal', show_hist=True, invert=False, interactive=False):
     if(invert):
         hist_data.reverse()
         group_labels.reverse()
     fig = ff.create_distplot(hist_data, group_labels, bin_size=bin_size,
                              curve_type=curve_type, show_hist=show_hist)
-    config = {'staticPlot': static}
-    fig.show(config=config)
+    if(interactive):
+        fig.show()
+    else:
+        return Image(pio.to_image(fig, format='png', width=1200))
 
 
-def hist(sr, interactive=False, static=True):
-  x = ["Id: " + str(i) for i in sr.index]
-  fig = px.histogram(x=x, y=sr.values)
-  config = {'staticPlot': static}
-  fig.show(config=config)
+def hist(sr, interactive=False):
+    x = ["Id: " + str(i) for i in sr.index]
+    fig = px.histogram(x=x, y=sr.values)
+    if(interactive):
+        fig.show()
+    else:
+        return Image(pio.to_image(fig, format='png', width=1200))
 
 
-def mult_hist(sr, rows, cols, title_text, subplot_titles, static=True):
-  fig = make_subplots(rows=rows, cols=cols, subplot_titles=subplot_titles)
-  for i in range(rows):
-    for j in range(cols):
-      x = ["-> " + str(i) for i in sr[i+j].index]
-      fig.add_trace(go.Bar(x=x, y=sr[i+j].values ), row=i+1, col=j+1)
-  fig.update_layout(showlegend=False, title_text=title_text)
-  config = {'staticPlot': static}
-  fig.show(config=config)
+def mult_hist(sr, rows, cols, title_text, subplot_titles, interactive=False):
+    fig = make_subplots(rows=rows, cols=cols, subplot_titles=subplot_titles)
+    for i in range(rows):
+        for j in range(cols):
+            x = ["-> " + str(i) for i in sr[i+j].index]
+            fig.add_trace(go.Bar(x=x, y=sr[i+j].values), row=i+1, col=j+1)
+    fig.update_layout(showlegend=False, title_text=title_text)
+    if(interactive):
+        fig.show()
+    else:
+        return Image(pio.to_image(fig, format='png', width=1200))
